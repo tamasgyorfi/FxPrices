07262351e79f6ea02bb63774e844798eba4b697d
tamasgyorfi <tamasgyorfi@bitbucket.org>
2015-11-27 17:11:29 +0100

    Quote factory for Yahoo prices refactored a bit (nested method => private methods)

--- FxPricer/src/hu/fx/service/providers/yahoo/xml/QuoteFactory.scala
+++ FxPricer/src/hu/fx/service/providers/yahoo/xml/QuoteFactory.scala
@@ -10,36 +10,32 @@ import hu.fx.service.providers.yahoo.config.YahooPriceSource
 class QuoteFactory extends YahooPriceSource {
 
   def fromNode(quote: Node): Quote = {
-    def createFxQuote(node: Node, currencyName: String): Quote = {
-      FxQuote(
-        currencyName.split("/")(1),
-        getAttribute(node, VOLUME_ATTRIBUTE) toInt,
-        getAttribute(node, PRICE_ATTRIBUTE) toDouble,
-        getAttribute(node, TIMESTAMP_ATTRIBUTE),
-        PROVIDER)
-    }
-
-    def createPmQuote(node: Node, currencyName: String): Quote = {
-      PmQuote(
-        currencyName,
-        getAttribute(node, VOLUME_ATTRIBUTE) toInt,
-        getAttribute(node, PRICE_ATTRIBUTE) toDouble,
-        getAttribute(node, TIMESTAMP_ATTRIBUTE),
-        PROVIDER)
-    }
-
-    def getAttribute(node: Node, attribute: String): String = {
-      (quote \\ "field").filter { node => (node \ ("@" + NAME_ATTRIBUTE)).text equals attribute } text
-    }
-
     val name = getAttribute(quote, NAME_ATTRIBUTE)
-    if (name.isEmpty) {
-      EmptyQuote
-    } else if (name.matches(FX_PATTERN)) {
-      createFxQuote(quote, name)
-    } else {
-      createPmQuote(quote, name)
-    }
+    if (name.isEmpty) EmptyQuote
+    else if (name.matches(FX_PATTERN)) createFxQuote(quote, name)
+    else createPmQuote(quote, name)
+  }
+  
+  private def createFxQuote(node: Node, currencyName: String): Quote = {
+		  FxQuote(
+				  currencyName.split("/")(1),
+				  getAttribute(node, VOLUME_ATTRIBUTE) toInt,
+				  getAttribute(node, PRICE_ATTRIBUTE) toDouble,
+				  getAttribute(node, TIMESTAMP_ATTRIBUTE),
+				  PROVIDER)
+  }
+  
+  private def createPmQuote(node: Node, currencyName: String): Quote = {
+		  PmQuote(
+				  currencyName,
+				  getAttribute(node, VOLUME_ATTRIBUTE) toInt,
+				  getAttribute(node, PRICE_ATTRIBUTE) toDouble,
+				  getAttribute(node, TIMESTAMP_ATTRIBUTE),
+				  PROVIDER)
+  }
+  
+  private def getAttribute(quote: Node, attribute: String): String = {
+		  (quote \\ "field").filter { node => (node \ ("@" + NAME_ATTRIBUTE)).text equals attribute } text
   }
 }
 
