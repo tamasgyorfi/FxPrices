2dbfa529382e7c604b7f9043c63875eb74e088fe
Tamas Gyorfi <Tamas_Gyorfi@epam.com>
2016-01-22 14:02:26 +0100

    refresh added for main screen

--- PresentationService/src/main/webapp/js/main.js
+++ PresentationService/src/main/webapp/js/main.js
@@ -8,47 +8,46 @@ $(document).on({
 });
 
 $(document).ready(function() {
-	
+
 	var currentPage = 0;
 	var allPages = 0;
 	var CURRENCIES_PER_PAGE = 20;
-	
+
 	var getParameterByName = function(name) {
-	    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-	    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-	        results = regex.exec(location.search);
-	    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+		name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+		var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
+		return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 	}
-	
+
 	var toPage = function(page) {
 		$('.pagination').fadeTo("fast", 1);
-		$('#pag_button'+page).fadeTo("fast", 0.5);		
+		$('#pag_button' + page).fadeTo("fast", 0.5);
 		$("#itemContainer").children().hide();
-		$('#itemContainer').children().slice(page*CURRENCIES_PER_PAGE, page*CURRENCIES_PER_PAGE+CURRENCIES_PER_PAGE).show();
+		$('#itemContainer').children().slice(page * CURRENCIES_PER_PAGE, page * CURRENCIES_PER_PAGE + CURRENCIES_PER_PAGE).show();
 	}
 
 	var setupPagination = function() {
 		$("#itemContainer").children().hide();
-		$('#itemContainer').children().slice(0, CURRENCIES_PER_PAGE).show(); 
-		
-		for (var i=0;i<=allPages;i++) {
-			$("#next").before("<button class='pagination' id='pag_button"+i+"'>"+(i+1)+"</button>")
+		$('#itemContainer').children().slice(0, CURRENCIES_PER_PAGE).show();
+
+		for ( var i = 0; i <= allPages; i++) {
+			$("#next").before("<button class='pagination' id='pag_button" + i + "'>" + (i + 1) + "</button>")
 		}
-		
+
 		$('#pag_button0').fadeTo("fast", 0.5);
 		$('.pagination').click(function() {
 			var id = $(this).attr('id');
 			var pageNr = id.substring("pag_button".length, id.length);
 			toPage(pageNr);
 		})
-		
+
 		$("#prev").click(function() {
 			if (currentPage !== 0) {
 				currentPage -= 1;
 				toPage(currentPage);
 			}
 		});
-		
+
 		$("#next").click(function() {
 			if (currentPage !== allPages) {
 				currentPage += 1;
@@ -56,14 +55,14 @@ $(document).ready(function() {
 			}
 		});
 	}
-	
+
 	var display = function(quotes) {
-		allPages = Math.floor(quotes.length / 20);
+		allPages = Math.floor(quotes.length / CURRENCIES_PER_PAGE);
 		for ( var i = 0; i < quotes.length; i++) {
 			var currencyPair = quotes[i].ccy1 + " / " + quotes[i].ccy2;
 			$("#placeholder").before("<div id='" + currencyPair + "' class='currency-button'><br/>" + currencyPair + "<br/> price: " + quotes[i].price + "</p>");
 		}
-		
+
 		$(".currency-button").mouseenter(function() {
 			$(this).fadeTo("fast", 1);
 		});
@@ -71,12 +70,18 @@ $(document).ready(function() {
 		$(".currency-button").mouseleave(function() {
 			$(this).fadeTo("fast", 0.5);
 		});
-		
+
 		setupPagination();
 	}
 
 	var vendor = getParameterByName("provider");
-	$("#itemContainer").before("<h2 class='h2' id='header'>Most fresh currencies for provider: " + vendor+" </h2>");
+	$("#itemContainer").before("<h2 class='h2' id='header'>Most fresh currencies for provider: " + vendor + " </h2>");
+
+	$("#refresh").click(function() {
+		location.reload();
+	});
+
+	$('html,body').css('cursor','default');
 	
 	$.ajax({
 		type : "GET",
@@ -94,7 +99,6 @@ $(document).ready(function() {
 			for (key in resp) {
 				x += key + " = " + resp[key] + "\n";
 			}
-			console.log(resp);
 			$("#error_box").fadeIn("slow", "linear");
 			$("#error_ok").click(function() {
 				$("#error_box").fadeOut("slow", "linear");
